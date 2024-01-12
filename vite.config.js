5f28276b0bbaf677b9babc7a008cc45e0f9e92ba
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2024-01-12 12:53:31 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default {
-  base: '/todo.project.io/'
-};
 export default defineConfig({
   plugins: [react()],
   base: '/todo.project.io/'
