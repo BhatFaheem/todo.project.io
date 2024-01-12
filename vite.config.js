d6a128a652c85107f0f9530c400b6a68c37d2b3d
Faheem Ul Islam <99001563+BhatFaheem@users.noreply.github.com>
2024-01-12 12:51:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+export default {
+  base: '/todo.project.io/'
+};
 export default defineConfig({
   plugins: [react()],
 })
